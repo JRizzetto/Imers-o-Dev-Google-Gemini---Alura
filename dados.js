63a73b8928dc8ec03409c5d08c76e5e36022bc2f
JRizzetto <104041862+JRizzetto@users.noreply.github.com>
2024-09-05 08:42:19 -0300

    Estilização e iniciação js

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+       {
+        Título: "Dudu (Eduardo Pereira Rodrigues)",
+        descricao: "Dudu, o camisa 7 do Palmeiras, é, indiscutivelmente, um dos maiores ídolos da história recente do clube.      Desde sua chegada em 2015, o atacante se tornou peça fundamental em diversas conquistas e estabeleceu uma série de recordes históricos. Com um total de 12 títulos, Dudu divide o posto de maior vencedor da história do Palmeiras com outros seis atletas. Seus números são impressionantes: mais de 450 jogos, quase 90 gols e mais de 100 assistências. No Allianz Parque, a casa alviverde, Dudu é o jogador com mais jogos, vitórias e assistências. O atacante se destaca não apenas pela quantidade de títulos, mas também pela qualidade de suas atuações. Dudu foi fundamental em diversas conquistas importantes, como os títulos brasileiros de 2016, 2018, 2022 e 2023, e as Libertadores de 2020 e 2021. Seu gol na final da Copa do Brasil de 2015, contra o Santos, é um dos mais marcantes da história do clube.",
+        link: "https://www.palmeiras.com.br/jogador/?jogador=2285"
+    },
+    {
+        Título: "Gustavo Gómez: O Capitão do Palmeiras",
+        descricao: "O zagueiro paraguaio Gustavo Gómez se consolidou como um dos maiores ídolos da história recente do Palmeiras. Desde sua chegada ao clube em 2018, o defensor se tornou peça fundamental em diversas conquistas e estabeleceu uma série de recordes históricos. Com um total de 12 títulos, Gómez divide o posto de maior vencedor da história do Palmeiras com outros seis atletas, incluindo o ídolo Ademir da Guia. Além disso, ele é o estrangeiro com mais títulos pelo Verdão e o capitão com mais taças conquistadas. Gómez é conhecido por sua solidez defensiva, liderança e faro de gol. Sua importância para o Palmeiras é inegável, tendo sido fundamental em diversas conquistas importantes, como os títulos brasileiros, as Libertadores e a Copa do Brasil. O zagueiro também se destaca por seus gols, especialmente em momentos decisivos.",
+        link: "https://www.palmeiras.com.br/jogador/?jogador=2297"
+    },
+    {
+        Título: "Weverton, A Muralha Verde",
+        descricao: "Gustavo Gómez é um zagueiro paraguaio que se tornou um dos maiores ídolos da história recente do Palmeiras. Desde sua chegada ao clube em 2018, o defensor se tornou peça fundamental em diversas conquistas e estabeleceu uma série de recordes históricos.Com um total de 12 títulos, Gómez divide o posto de maior vencedor da história do Palmeiras com outros seis atletas, incluindo o ídolo Ademir da Guia. Além disso, ele é o estrangeiro com mais títulos pelo Verdão e o capitão com mais taças conquistadas.Gómez é conhecido por sua solidez defensiva, liderança e faro de gol. Sua importância para o Palmeiras é inegável, tendo sido fundamental em diversas conquistas importantes, como os títulos brasileiros, as Libertadores e a Copa do Brasil. O zagueiro também se destaca por seus gols, especialmente em momentos decisivos.",
+        link: "https://www.palmeiras.com.br/jogador/?jogador=2284"
+    }
+];
+
+
