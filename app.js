fef95ce7010ce563b915ebf82b3a8c885401ee28
JRizzetto <104041862+JRizzetto@users.noreply.github.com>
2024-09-05 09:46:11 -0300

    Adicionando dados no JS e botão pesquisar

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log(dados);
+function pesquisar(){
+    // Seleciona a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+  
+    // Itera sobre cada objeto no array de dados
+    for(let dado of dados){
+      // Constrói o HTML para cada resultado, incluindo título, descrição e link
+      resultados += `
+        <div class="item-resultado">
+          <h2>${dado.Título}</h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Saiba mais</a>
+        </div>
+      `;
+    }
+  
+    // Atualiza o conteúdo da seção de resultados com o HTML construído
+    section.innerHTML = resultados;
+  }
+
+
+
+
+
