480f67b3f3275b3dcb21dd8e43fc0ad2f300b7d5
JRizzetto <104041862+JRizzetto@users.noreply.github.com>
2024-09-06 10:40:06 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
 function pesquisar(){
     // Seleciona a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // Se a pesquisa for vazia, faça:
+    if (campoPesquisa == ""){
+      section.innerHTML = "O campo de busca está vazio"
+      return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
   
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
-  
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
     // Itera sobre cada objeto no array de dados
     for(let dado of dados){
+      titulo = dado.titulo.toLocaleLowerCase();
+      descricao = dado.descricao.toLocaleLowerCase();
+      tags = dado.tags.toLocaleLowerCase();
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+          
       // Constrói o HTML para cada resultado, incluindo título, descrição e link
       resultados += `
         <div class="item-resultado">
-          <h2>${dado.Título}</h2>
+          <h2>${dado.titulo}</h2>
           <p class="descricao-meta">${dado.descricao}</p>
           <a href="${dado.link}" target="_blank">Saiba mais</a>
         </div>
       `;
     }
+  }
+
+    // caso as buscas não encontrem nada
+    if(!resultados) {
+      resultados = "<p>O resultado de busca não obteve resultados</p>"
+    }
   
     // Atualiza o conteúdo da seção de resultados com o HTML construído
     section.innerHTML = resultados;
   }
-
+  
 
 
 
--- dados.js
+++ dados.js
@@ -1,18 +1,21 @@
 let dados = [
        {
-        Título: "Dudu (Eduardo Pereira Rodrigues)",
+        titulo: "Dudu (Eduardo Pereira Rodrigues)",
         descricao: "Dudu, o camisa 7 do Palmeiras, é, indiscutivelmente, um dos maiores ídolos da história recente do clube.      Desde sua chegada em 2015, o atacante se tornou peça fundamental em diversas conquistas e estabeleceu uma série de recordes históricos. Com um total de 12 títulos, Dudu divide o posto de maior vencedor da história do Palmeiras com outros seis atletas. Seus números são impressionantes: mais de 450 jogos, quase 90 gols e mais de 100 assistências. No Allianz Parque, a casa alviverde, Dudu é o jogador com mais jogos, vitórias e assistências. O atacante se destaca não apenas pela quantidade de títulos, mas também pela qualidade de suas atuações. Dudu foi fundamental em diversas conquistas importantes, como os títulos brasileiros de 2016, 2018, 2022 e 2023, e as Libertadores de 2020 e 2021. Seu gol na final da Copa do Brasil de 2015, contra o Santos, é um dos mais marcantes da história do clube.",
-        link: "https://www.palmeiras.com.br/jogador/?jogador=2285"
+        link: "https://www.palmeiras.com.br/jogador/?jogador=2285",
+        tags: "atacante, ponta, goleador, multicampeão, ídolo, pé esquerdo"
     },
     {
-        Título: "Gustavo Gómez: O Capitão do Palmeiras",
+        titulo: "Gustavo Gómez: O Capitão do Palmeiras",
         descricao: "O zagueiro paraguaio Gustavo Gómez se consolidou como um dos maiores ídolos da história recente do Palmeiras. Desde sua chegada ao clube em 2018, o defensor se tornou peça fundamental em diversas conquistas e estabeleceu uma série de recordes históricos. Com um total de 12 títulos, Gómez divide o posto de maior vencedor da história do Palmeiras com outros seis atletas, incluindo o ídolo Ademir da Guia. Além disso, ele é o estrangeiro com mais títulos pelo Verdão e o capitão com mais taças conquistadas. Gómez é conhecido por sua solidez defensiva, liderança e faro de gol. Sua importância para o Palmeiras é inegável, tendo sido fundamental em diversas conquistas importantes, como os títulos brasileiros, as Libertadores e a Copa do Brasil. O zagueiro também se destaca por seus gols, especialmente em momentos decisivos.",
-        link: "https://www.palmeiras.com.br/jogador/?jogador=2297"
+        link: "https://www.palmeiras.com.br/jogador/?jogador=2297",
+        tags: "zagueiro, capitão, líder, forte, aéreo, multicampeão"
     },
     {
-        Título: "Weverton, A Muralha Verde",
+        titulo: "Weverton, A Muralha Verde",
         descricao: "Weverton se tornou um dos maiores goleiros da história do Palmeiras. Desde sua chegada ao clube, o arqueiro conquistou diversos títulos e se consolidou como uma peça fundamental da equipe. Com uma série de recordes, como o maior número de jogos sem sofrer gols neste século e a menor média de gols sofridos por partida, Weverton é considerado um dos goleiros mais seguros do futebol brasileiro. Além de sua excelência dentro de campo, Weverton também é um líder nato e um ídolo da torcida palmeirense. Sua importância para o Palmeiras é inegável, tendo sido fundamental em diversas conquistas importantes, como os títulos brasileiros, as Libertadores e a Copa do Brasil. O goleiro também se destaca por sua precisão nas reposições de bola, sendo um dos pontos fortes do time no início das jogadas. Com sua segurança, liderança e títulos conquistados, Weverton se tornou uma lenda viva do Palmeiras e um dos maiores goleiros da história do futebol brasileiro.",
-        link: "https://www.palmeiras.com.br/jogador/?jogador=2284"
+        link: "https://www.palmeiras.com.br/jogador/?jogador=2284",
+        tags: "goleiro, muralha, seguro, líder, reflexo, multicampeão"
     }
 ];
 
